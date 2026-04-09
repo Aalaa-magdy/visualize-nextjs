ce8e0e4850d863adc586ffa2f33d57da06d22ac4
Aalaa-magdy <am7371494@gmail.com>
2026-04-09 23:21:26 +0200

    work with chadcn ui library

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex min-h-screen flex-col items-center justify-between p-24 text-red-500">
            start a big visulization project for analysing all react/next js concepts 
     </div>
   );
