8028a7e5ea690a34c17f7404558ca4aaeaa4df52
Aalaa-magdy <am7371494@gmail.com>
2026-04-10 19:44:19 +0200

    feat: start ui with shadcn/ dark light mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import { ModeToggle } from "@/components/ui/toggle";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24 text-red-500">
-           start a big visulization project for analysing all react/next js concepts 
+           <ModeToggle />
     </div>
   );
 }
---
+++ components/ui/toggle.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import * as React from "react"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = React.useState(false)
+
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
+
+  const isDark = theme === "dark"
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          {isDark ? (
+            <Sun className="h-[1.2rem] w-[1.2rem]" />
+          ) : (
+            <Moon className="h-[1.2rem] w-[1.2rem]" />
+          )}
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
